d7b56573590006ca0694621af873fdf6555cd341
Arnab199993 <103571005+Arnab199993@users.noreply.github.com>
2022-12-10 14:45:40 +0530

    Todo Final

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,197 +1,201 @@
 import React from 'react'
 import { useState } from 'react'
-
-
-
-const App = (props) => {
+// import dateDifference from "./Date"
+const Todo = (props) => {
   const [state, setState] = useState(false)
-  const colorchange = () => {
-    setState(!state)
-  }
+const colorchange=()=>{
+  setState(!state)
+}
+  const dateDifference = (date) => {
+    return moment().diff(date, 'days');
+  };
+  //  const [states, setStates] = useState([
+  //   {
+  //     date: '2022-01-01',
+  //   },
+  //   {
+  //     date: '2022-09-01',
+  //   },
+  //   {
+  //     date: '2022-12-01',
+  //   },
+  // ]);
+  
+
+  
+   const defaultState={
+
+    name:"",
+    date:"",
+    index:0,
+    id:"",
+    completed:false
+   }
 
 
-  const [todos, setTodos] = React.useState([
-    {
-      text: "This is a sample todo",
-      isDone: false
-    }
-  ]);
-
-
-
-  const markTodo = index => {
-    const newTodos = [...todos];
-    newTodos[index].isDone = true;
-    setTodos(newTodos);
+const checkboxeditdata = (index) => {
+    let item = itemsarray[index];
+    item.completed = true;
+    itemsarray[index] = item;
+    setItemsArray([...itemsarray]);
   };
 
 
 
 
+  
+  
+   const  [count,setCount]=useState(0);
+   const [editMode,setEditMode]=useState(false);
+
+   const [task,setTask]=useState(defaultState)
+   
+    const [itemsarray,setItemsArray]=useState([]);
+
+   const inputChange=(event)=>{
 
 
+    setTask({...task,[event.target.name]:event.target.value});
 
+   }
 
-  const defaultState = {
-
-    name: "",
-    date: "",
-    index: 0,
-    id: ""
-  }
-
-  const [count, setCount] = useState(0);
-  const [editMode, setEditMode] = useState(false);
-
-  const [task, setTask] = useState(defaultState)
-
-  const [itemsarray, setItemsArray] = useState([]);
-
-  const inputChange = (event) => {
-
-
-    setTask({ ...task, [event.target.name]: event.target.value });
-
-  }
-
-  const addToItemArray = () => {
+   const addToItemArray=()=>{
     itemsarray.push(task);
     setItemsArray([...itemsarray]);
 
     setTask(defaultState)
 
-  }
-  const removeItem = (index) => {
-    itemsarray.splice(index, 1);
+   }
+   const removeItem=(index)=>{
+   itemsarray.splice(index,1);  
 
-    let newArr = [];
-    for (let i = 0; i < itemsarray.length; i++) {
-      newArr.push(itemsarray[i]);
-    }
+   let newArr=[];  
+   for (let i=0;i<itemsarray.length;i++){
+    newArr.push(itemsarray[i]);
+   }
     setItemsArray(newArr);
 
-  }
-  const populateEditData = (ele, i) => {
+   }
+   const populateEditData=(ele,i)=>{
 
     setTask({
 
-      name: ele.name,
-      date: ele.date,
-      index: i,
-      id: ele.id
-    })
-    setEditMode(true);
-  }
+      name:ele.name,
+      date:ele.date,
+      index:i,
+      id:ele.id
+     })
+     setEditMode(true);
+   }
 
-  const editItem = () => {
+   const editItem=()=>{
 
     console.log(task);
-    const filteredData = itemsarray.filter(ele => ele.id !== task.id)
+const filteredData=itemsarray.filter(ele=>ele.id!==task.id)
 
-    filteredData.push(task);
-    setItemsArray([...filteredData]);
-    setEditMode(false);
+filteredData.push(task);
+setItemsArray([...filteredData]);
+setEditMode(false); 
 
 
-  }
+   }
 
 
   return (
-    <div className={"container"} style={{ textAlign: "center" }}>
+    <div className={"container"} style={{textAlign:"center"}}>
+
+      
 
 
+      
+     
+    
 
 
+      
+        <h1 style={{textAlign:"center"}}>My ToDo App</h1>
+        <div className={"row"} style={{marginTop:"40px"}}>
 
+            <div className={'col-md-6'}>
+                <input type={"text"} className={"form-control"} value={task.name}  name={"name"} placeholder={"Task Name"} onChange={inputChange}/> <br></br>
+                <input type={"date"} className={"form-control"}  value={task.date} name={"date"}  placeholder={"Task End Date"} onChange={inputChange}/> <br/>
 
+               {
+                 
+                editMode?
 
-      <h1 style={{ textAlign: "center" }}>My ToDo App</h1>
-      <div className={"row"} style={{ marginTop: "40px" }}>
-
-        <div className={'col-md-6'}>
-          <input type={"text"} className={"form-control"} value={task.name} name={"name"} placeholder={"Task Name"} onChange={inputChange} /> <br></br>
-          <input type={"date"} className={"form-control"} value={task.date} name={"date"} placeholder={"Task End Date"} onChange={inputChange} /> <br />
-
-          {
-
-            editMode ?
-
-              <button className={"btn btn-primary"} onClick={editItem}>
-                Edit ToDo
-</button> : <button className={"btn btn-primary"} onClick={addToItemArray}>
-                Add ToDo
+<button className={"btn btn-primary"} onClick={editItem}>
+Edit ToDo
+</button> : <button  className={"btn btn-primary"} onClick={addToItemArray}>
+Add ToDo
 </button>
-          }
+               }
+
+            
+
+           
+
+            </div>
 
 
+            <div className={'col-md-6'}>
+            <ul className={"list-group"}>
 
+                {
+                    itemsarray.map((ele,i)=>(  
+                    
+                       <li  
 
+                         style={ele.completed ? { textDecoration: "line-through" } : {}} key={i}
+                                
+                         
+      className="list-group-item" aria-current="true"><strong>Activity - </strong>{ele.name}   <strong>Finish Date -</strong> {ele.date} 
+                                  <input
+                                    key={i} 
+              type="checkbox"
+              onChange={() => checkboxeditdata(i)}                         
+            />
+                         
+                                      
+                       <button className={"btn btn-danger"} style={{marginLeft:"20px"}} onClick={()=>removeItem(i)}>
+                        Delete
+                       </button>
+             
 
-        </div>
+                       <button className={"btn btn-danger"} style={{marginLeft:"20px"}} onClick={function (){
 
+return populateEditData(ele,i);
 
-        <div className={'col-md-6'}>
-          <ul className={"list-group"}>
+}}>
 
-            {
-              itemsarray.map((ele, i) => (
-
-                <li
-
-
-
-
-
-
-
-                  className="list-group-item" aria-current="true"><strong>Activity - </strong>{ele.name}   <strong>Finish Date -</strong> {ele.date}
-                  <button className={"btn btn-danger"} style={{ marginLeft: "20px" }} onClick={() => removeItem(i)}>
-                    Delete
+              
+                        Edit
                        </button>
 
-                  <button className={"btn btn-danger"} style={{ marginLeft: "20px" }} onClick={function() {
-
-                    return populateEditData(ele, i);
-
-                  }}>
-                    Edit
-                       </button>
+                     
+                       
+                       </li>
 
 
+                    ))
+                }
 
-                  <input type={"checkbox"} onClick={() => markTodo(index)}></input>
+</ul>
 
-
-
-
-
-                </li>
-
-
-              ))
-            }
-
-
-
-
-          </ul>
-
-
-
+            </div>
 
         </div>
-
-
-      </div>
-
       <button
         onMouseOver={colorchange}
         style={{ backgroundColor: state ? "yellow" : "red" }}
       >
         Color Change
       </button>
+      
+
+<dateDifference/>
     </div>
   )
 }
 
-export default App
+export default Todo
